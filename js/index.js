9c5909b29639a91ed578e57c44ea705a13409d6f
Sean Alvarez <alvie0818@gmail.com>
2015-11-06 14:03:09 -0500

    Ignore generated files

--- js/index.js
+++ js/index.js
@@ -1,36 +1,30 @@
-var weather;
-(function (weather_1) {
-    var getWeather = (function () {
-        function getWeather() {
-        }
-        getWeather.prototype.getCurrentWeatherByLatLong = function (lat, lon) {
-            var weather;
-            var urlLoc = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon;
-            $.getJSON(urlLoc, parseWeatherData);
-            return weather;
-        };
-        getWeather.prototype.getCurrentWeatherByZipCode = function (zipCode) {
-            var weather;
-            $.getJSON('api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us', parseWeatherData);
-            return weather;
-        };
-        return getWeather;
-    })();
-    weather_1.getWeather = getWeather;
-    function parseWeatherData(data) {
-        var weather = data;
-        return weather;
-    }
-})(weather || (weather = {}));
 /// <reference path="jquery.d.ts" />
-/// <reference path="weather.ts" />
 $(function () {
-    console.log('Getting location...');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             console.log(position.coords.latitude, ' ', position.coords.longitude);
-            var weather = new weather.getWeather();
+            var weather = new getWeather();
             weather.getCurrentWeatherByLatLong(position.coords.latitude, position.coords.longitude);
         });
     }
 });
+var getWeather = (function () {
+    function getWeather() {
+    }
+    getWeather.prototype.getCurrentWeatherByLatLong = function (lat, lon) {
+        var weather;
+        var urlLoc = 'http://api.openweathermap.org/data/2.5/weather?appid=f9402e85504b0c5500f6f9cd39e19394&lat=' + lat + '&lon=' + lon;
+        $.getJSON(urlLoc, function (data) {
+            var weather = data;
+            alert(JSON.stringify(data));
+        });
+    };
+    getWeather.prototype.getCurrentWeatherByZipCode = function (zipCode) {
+        var weather;
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?appid=f9402e85504b0c5500f6f9cd39e19394&zip=' + zipCode + ',us', function (data) {
+            var weather = data;
+        });
+    };
+    return getWeather;
+})();
+//# sourceMappingURL=index.js.map
