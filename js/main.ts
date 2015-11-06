053cb567a882fe5f8574dd04b42b101d0df9b576
Sean Alvarez <alvie0818@gmail.com>
2015-11-06 14:01:51 -0500

    Converted to a VS 2015 solution

--- js/main.ts
+++ js/main.ts
@@ -1,11 +1,33 @@
 /// <reference path="jquery.d.ts" />
-/// <reference path="weather.ts" />
 $(function() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			console.log(position.coords.latitude, ' ', position.coords.longitude);
-			var weather = new weather.getWeather();
+			var weather = new getWeather();
 			weather.getCurrentWeatherByLatLong(position.coords.latitude, position.coords.longitude);
 		});
 	}
 })
+
+class getWeather {
+	getCurrentWeatherByLatLong(lat: number, lon: number) {
+		var weather: curWeather;
+		var urlLoc: string = 'http://api.openweathermap.org/data/2.5/weather?appid=f9402e85504b0c5500f6f9cd39e19394&lat=' + lat + '&lon=' + lon;
+		$.getJSON(urlLoc, function(data) {
+			var weather: curWeather = data;
+			alert(JSON.stringify(data));
+			
+		});
+	}
+
+	getCurrentWeatherByZipCode(zipCode: number) {
+		var weather: curWeather;
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?appid=f9402e85504b0c5500f6f9cd39e19394&zip=' + zipCode + ',us', function (data) {
+			var weather: curWeather = data;
+		});
+	}
+}
+
+interface curWeather {
+
+}
